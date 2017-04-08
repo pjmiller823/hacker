6080c83827e397201f565eb9ba0c23e26c20b817
Patrick Miller <pjmiller823@gmail.com>
2017-04-07 21:15:14 -0400

    didn't save previous file. http and https validation

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,5 @@
 class Story < ApplicationRecord
   validates :link, presence: true
   validates :email, presence: true
-  # validates_format_of :link, with: :URI.regexp(%w{http https})
   validates :link, format: { with: /\A(http:|https)/ }
 end
