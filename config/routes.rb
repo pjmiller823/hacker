71dec9aa76afb99e00d90726b7050a7acbc7d9fd
Patrick Miller <pjmiller823@gmail.com>
2017-04-11 13:15:44 -0400

    uncommented bcrypt, and generated a user model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :stories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/signup' => 'users#new'
+  post '/users' => 'users#creat'
 end
