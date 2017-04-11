58a26f3fa84ab1b03867bfbc3963ddda4da6c433
Patrick Miller <pjmiller823@gmail.com>
2017-04-11 17:39:39 -0400

    Removed the email column from stories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170411200459) do
+ActiveRecord::Schema.define(version: 20170411213711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 20170411200459) do
   create_table "stories", force: :cascade do |t|
     t.string   "title"
     t.string   "link"
-    t.string   "email"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "created_by_id"
