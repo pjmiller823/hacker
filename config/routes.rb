e8fbb9adfdbfd48c714ba2e9c8ab81e15a5b3516
Patrick Miller <pjmiller823@gmail.com>
2017-05-06 20:30:02 -0400

    moved index to root page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     resources :comments
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'stories#index'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
   get '/login'  => 'sessions#new'
